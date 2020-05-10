85b8a8f339ebf1b1f2bbf76bd7720d909f08f44d
yunqi.zhou <yunqi.zhou@myob.com>
2020-05-10 23:45:55 +0800

    formatting

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz_v1.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz_v1.java
@@ -6,64 +6,63 @@ public class FizzBuzzWhizz_v1 {
 
         String saying = "";
 
-        if (doesContain(i,7)){
-            if (isMultipleOf(i,3)){
-                saying = append(saying,"Fizz");
+        if (doesContain(i, 7)) {
+            if (isMultipleOf(i, 3)) {
+                saying = append(saying, "Fizz");
             }
-            if (doesContain(i,5)){
-                if (isMultipleOf(i,7)){
-                    saying = append(saying,"Whizz");
+            if (doesContain(i, 5)) {
+                if (isMultipleOf(i, 7)) {
+                    saying = append(saying, "Whizz");
                 }
-            }else {
-                if (!doesContain(i,3)){
-                    if (isMultipleOf(i,7)){
-                        saying = append(saying,"Whizz");
+            } else {
+                if (!doesContain(i, 3)) {
+                    if (isMultipleOf(i, 7)) {
+                        saying = append(saying, "Whizz");
                     }
                 }
             }
-
         }
 
-        if (doesContain(i,5) && !doesContain(i,7)){
-            if (isMultipleOf(i,5)){
-                saying = append(saying,"Buzz");
+        if (doesContain(i, 5) && !doesContain(i, 7)) {
+            if (isMultipleOf(i, 5)) {
+                saying = append(saying, "Buzz");
             }
-            if (isMultipleOf(i,7)){
-                saying = append(saying,"Whizz");
+            if (isMultipleOf(i, 7)) {
+                saying = append(saying, "Whizz");
             }
         }
 
-        if (doesContain(i,3) && !doesContain(i,5) && !doesContain(i,7)){
-            saying = append(saying,"Fizz");
+        if (doesContain(i, 3) && !doesContain(i, 5) && !doesContain(i, 7)) {
+            saying = append(saying, "Fizz");
         }
-        if (!doesContain(i,3) && !doesContain(i,5) && !doesContain(i,7)){
-            if (isMultipleOf(i,3)){
-                saying = append(saying,"Fizz");
+        if (!doesContain(i, 3) && !doesContain(i, 5) && !doesContain(i, 7)) {
+            if (isMultipleOf(i, 3)) {
+                saying = append(saying, "Fizz");
             }
-            if (isMultipleOf(i,5)){
-                saying = append(saying,"Buzz");
+            if (isMultipleOf(i, 5)) {
+                saying = append(saying, "Buzz");
             }
-            if (isMultipleOf(i,7)){
-                saying = append(saying,"Whizz");
+            if (isMultipleOf(i, 7)) {
+                saying = append(saying, "Whizz");
             }
         }
 
         return saying.length() == 0 ? String.valueOf(i) : saying;
     }
 
-    private String append(String str, String appending){
+    private String append(String str, String appending) {
         return str + appending;
     }
 
-    private String intToString(int i){
+    private String intToString(int i) {
         return String.valueOf(i);
     }
 
-    private boolean doesContain(int count, int matcher){
+    private boolean doesContain(int count, int matcher) {
         return intToString(count).contains(intToString(matcher));
     }
 
-    private boolean isMultipleOf(int count, int divider){
+    private boolean isMultipleOf(int count, int divider) {
         return count % divider == 0;
     }
 }
--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz_v1Test.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz_v1Test.java
@@ -11,37 +11,37 @@ public class FizzBuzzWhizz_v1Test {
     private FizzBuzzWhizz_v1 subject = new FizzBuzzWhizz_v1();
 
     @Test
-    public void givenCountIsAMultipleOf3_whenCountOff_shouldReturnFizz(){
+    public void givenCountIsAMultipleOf3_whenCountOff_shouldReturnFizz() {
         String result = subject.countOff(6);
         Assertions.assertThat(result).isEqualTo("Fizz");
     }
 
     @Test
-    public void givenCountIsAMultipleOf5_whenCountOff_shouldReturnBuzz(){
+    public void givenCountIsAMultipleOf5_whenCountOff_shouldReturnBuzz() {
         String result = subject.countOff(10);
         Assertions.assertThat(result).isEqualTo("Buzz");
     }
 
     @Test
-    public void givenCountIsAMultipleOf7_whenCountOff_shouldReturnWhizz(){
+    public void givenCountIsAMultipleOf7_whenCountOff_shouldReturnWhizz() {
         String result = subject.countOff(14);
         Assertions.assertThat(result).isEqualTo("Whizz");
     }
 
     @Test
-    public void givenCountIsAMultipleOf3And5_whenCountOff_shouldReturnFizzBuzz(){
+    public void givenCountIsAMultipleOf3And5_whenCountOff_shouldReturnFizzBuzz() {
         String result = subject.countOff(60);
         Assertions.assertThat(result).isEqualTo("FizzBuzz");
     }
 
     @Test
-    public void givenCountIsAMultipleOf3And7_whenCountOff_shouldReturnFizzWhizz(){
+    public void givenCountIsAMultipleOf3And7_whenCountOff_shouldReturnFizzWhizz() {
         String result = subject.countOff(21);
         Assertions.assertThat(result).isEqualTo("FizzWhizz");
     }
 
     @Test
-    public void givenCountIsAMultipleOf7And5_whenCountOff_shouldReturnBuzzWhizz(){
+    public void givenCountIsAMultipleOf7And5_whenCountOff_shouldReturnBuzzWhizz() {
         String result = subject.countOff(140);
         Assertions.assertThat(result).isEqualTo("BuzzWhizz");
     }
@@ -53,55 +53,55 @@ public class FizzBuzzWhizz_v1Test {
     }
 
     @Test
-    public void givenCountIsNotMultipleOf3_Or_5_Or_7_whenCountOff_shouldReturnCount(){
+    public void givenCountIsNotMultipleOf3_Or_5_Or_7_whenCountOff_shouldReturnCount() {
         String result = subject.countOff(1);
         Assertions.assertThat(result).isEqualTo("1");
     }
 
     @Test
-    public void givenCountContains3_whenCountOff_shouldReturnFizz(){
+    public void givenCountContains3_whenCountOff_shouldReturnFizz() {
         String result = subject.countOff(13);
         Assertions.assertThat(result).isEqualTo("Fizz");
     }
 
     @Test
-    public void givenCountContains3_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizz(){
+    public void givenCountContains3_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizz() {
         String result = subject.countOff(630);
         Assertions.assertThat(result).isEqualTo("Fizz");
     }
 
     @Test
-    public void givenCountContains5_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnBuzzWhizz(){
+    public void givenCountContains5_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnBuzzWhizz() {
         String result = subject.countOff(315);
         Assertions.assertThat(result).isEqualTo("BuzzWhizz");
     }
 
     @Test
-    public void givenCountContains7_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizzWhizz(){
+    public void givenCountContains7_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizzWhizz() {
         String result = subject.countOff(1470);
         Assertions.assertThat(result).isEqualTo("FizzWhizz");
     }
 
     @Test
-    public void givenCountContains7and3_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizz(){
+    public void givenCountContains7and3_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizz() {
         String result = subject.countOff(2730);
         Assertions.assertThat(result).isEqualTo("Fizz");
     }
 
     @Test
-    public void givenCountContains7and3_AlsoIsMultipleOf5and7_whenCountOff_shouldReturnFizz(){
+    public void givenCountContains7and3_AlsoIsMultipleOf5and7_whenCountOff_shouldReturnFizz() {
         String result = subject.countOff(3710);
         Assertions.assertThat(result).isEqualTo("3710");
     }
 
     @Test
-    public void givenCountContains7and5_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizzWhizz(){
+    public void givenCountContains7and5_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizzWhizz() {
         String result = subject.countOff(1575);
         Assertions.assertThat(result).isEqualTo("FizzWhizz");
     }
 
     @Test
-    public void givenCountContains7and5an3_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizzWhizz(){
+    public void givenCountContains7and5an3_AlsoIsMultipleOf3and5and7_whenCountOff_shouldReturnFizzWhizz() {
         String result = subject.countOff(735);
         Assertions.assertThat(result).isEqualTo("FizzWhizz");
     }
@@ -117,8 +117,6 @@ public class FizzBuzzWhizz_v1Test {
 //        }
 //        }
 //    }
-
-
 
 
 }
